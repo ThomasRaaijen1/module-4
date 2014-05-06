dc3c6c5f793d3deed08e9074dd5efd357e178552
Thommie94boy <t.raaijen@live.nl>
2014-05-06 16:05:06 +0200

    exercise3b.sql stap 1 shunting rule

---
+++ datainfo/session2/exercise3b.sql
@@ -0,0 +1,10 @@
+--stap 1 shunting rule
+
+SELECT p.pid FROM Person p 
+ WHERE EXISTS( 
+	SELECT * 
+	FROM Acts a, Movies m 
+	WHERE a.pid = p.pid
+	AND m.mid = a.mid 
+	AND m.name = 'Back to the Future'); 
+	
