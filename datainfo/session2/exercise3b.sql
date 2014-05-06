20cc06fda23a2bc8c9606f6fb311d40e42a838e4
Thommie94boy <t.raaijen@live.nl>
2014-05-06 16:06:17 +0200

    exercise3b.sql stap 2 shunting rule

--- datainfo/session2/exercise3b.sql
+++ datainfo/session2/exercise3b.sql
@@ -8,3 +8,11 @@ SELECT p.pid FROM Person p
 	AND m.mid = a.mid 
 	AND m.name = 'Back to the Future'); 
 	
+--stap 2 shunting rule
+
+SELECT DISTINCT p.pid 
+ FROM Person p, Acts a, Movies m 
+ WHERE a.pid = p.pid
+ AND m.mid = a.mid 
+ AND m.name = 'Back to the Future';
+ 
